ef8099d94f9c4a5f460572daa0dff63223cb0e99
Parasbuda <parasbuda1234@gmail.com>
2021-02-17 17:26:31 +0545

    initial basics

---
+++ add.py
@@ -0,0 +1,73 @@
+# a="abc"
+# b="def"
+# sum=a+b
+# print(sum)
+# a=1
+# b=2
+# s=a+b+3
+# print(s)
+# a=-2
+# a=float(a)
+# print(type(a))
+# if a>0:
+#     print("Number is positive")
+# elif a<0:
+#     print("Number is negative")
+# else:
+#     print("Number is Zero")
+
+# x=y=z="paras"
+# a=x+y+z
+# print(a)
+
+# names=["paras","yuviii"]
+# x,y=names
+# print(x,y)
+# a="handsome"
+# print("Paras is " +a)
+# a="paras"
+# def name():
+#    global b
+#    b="yuvii"
+#    print("My name is "+b)
+
+# name()
+# print("My name is "+b)
+
+# arr={'a':1,'b':2,'c':3}
+# arr={'a','b','c'}
+# arr=range(10)
+# print(type(arr))
+name="paras, buda"
+# for x in name:
+#     print(x)
+
+# print("ra" in name)
+# if 'xra' not in name:
+#     print("Yes present")
+# print(name[-2:-1])
+# print(name.upper())
+# print(name.strip())
+# print(name.replace('R','r'))
+# print(name.split(','))
+# txt="My favourite food is \"momo\""
+# print(txt)
+
+# a="My name is parasbuda1234gmailcom"
+# print(a.capitalize())
+# print(a.casefold())
+# print(a.count('p'))
+# print(a.encode())
+# print(a.endswith("x"))
+# print(a.endswith("m"))
+# print(a.find('p'))
+# print(a.index('p'))
+# print(a.isalnum())
+# print(a.isalpha())
+# print(a.isdecimal())
+# print(a.isdigit())
+# print(a.isidentifier())
+
+arr=["paras","nirmal"]
+arr[1]="yuvii"
+print(arr)
